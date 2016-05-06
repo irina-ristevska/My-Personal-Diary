33b3fdcd0bf352dd0409093880e4bc73554ecedf
Irina <irina.rist3vska@gmail.com>
2016-05-06 19:49:08 +0200

    Dodadena LogIn Form i UserBaseController

---
+++ My Personal Diary/My Personal Diary/UserDiary.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace My_Personal_Diary
+{
+    public partial class UserDiary : Form
+    {
+        string DiaryFile
+        {
+            get; set;
+        }
+        string UserName { get; set; }
+
+        public void setDiaryFile(string url, string user)
+        { DiaryFile = url; UserName = user; }
+
+        public UserDiary()
+        {
+            InitializeComponent();
+           
+        }
+
+        
+    }
+}
